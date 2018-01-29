9fe7fc5ec37ce9492fea3dcd8f50be1e90518b01
Shuan Wang <shuanwang@gmail.com>
2018-01-28 16:28:57 -0800

    forgot new dist/bundle

--- dist/bundle.js
+++ dist/bundle.js
@@ -1148,40 +1148,45 @@ var imgCache = {};
 var g = window.location.href;
 var githubUrl = g.endsWith('/index.html') ? g.slice(0, -10) : g;
 
+var mp3s = ['Pharah_-_Defend_The_Point,_Strike_As_One.mp3', 'Pharah_-_I\'m_Impressed.mp3', 'Pharah_-_I\'m_Taking_The_Objective,_Converge_On_Me.mp3', 'Pharah_-_I_Always_Get_My_Prey.mp3', 'Pharah_-_Impressed.mp3', 'Pharah_-_Let\'s_Keep_The_Payload_Moving.mp3', 'Pharah_-_My_Ultimate_Is_Ready.mp3', 'Pharah_-_Remember_Your_Training,_And_We\'ll_Get_Through_This_Just_Fine.mp3', 'Pharah_-_Tango,_Down.mp3'];
 var url = 'Pharah_-_Impressed.mp3';
+var de = document.getElementById.bind(document);
+var f = new FetchBase64();
 
-var ll = function ll() {
+var cacheAudio = function cacheAudio(url) {
+  return new Promise(function (resolve, reject) {
+    if (cache[url]) {
+      resolve(cache[url]);
+    } else {
+      f.fetch(githubUrl + 'mp3/' + url, { mode: 'no-cors' }).then(function (base64) {
+        cache[url] = base64;
+        resolve(base64);
+      }).catch(function (err) {
+        de('debug').innerText = JSON.stringify(err);
+        console.error(err);
+        reject(err);
+      });
+    }
+  });
+};
 
-  var f = new FetchBase64();
-  // let url = 'Pharah_-_Justice_rains_from_above!.ogg';
-  if (cache[url]) {
-    // alert('already fetched and cached audio ' + url)
-    // document.getElementById('source').src = cache[url];
-    // document.getElementById('yoaudio').load();
+var cacheMedia = function cacheMedia(url) {
+
+  // let icon = 'pharah_front_icon.jpg'
+  var icon = 'favicon.ico';
+  if (imgCache[icon]) {
+    de('pharah_icon').src = imgCache[icon];
   } else {
-    f.fetch(githubUrl + url, { mode: 'no-cors' }).then(function (base64) {
-      // document.getElementById('source').src = base64;
-      cache[url] = base64;
-      // document.getElementById('yoaudio').load();
-      // alert('fetched and cached audio ' + url)
-    }).catch(function (err) {
-      document.getElementById('debug').innerText = JSON.stringify(err);
-      console.error(err);
-    });
-  }
-
-  if (imgCache['pharah_icon.jpg']) {
-    document.getElementById('pharah_icon').src = imgCache['pharah_icon.jpg'];
-  } else {
-    f.fetchAsData(githubUrl + 'pharah_icon.jpg').then(function (dataUri) {
-      document.getElementById('pharah_icon').src = dataUri;
-      imgCache['pharah_icon.jpg'] = dataUri;
+    f.fetchAsData(githubUrl + icon).then(function (dataUri) {
+      de('pharah_icon').src = dataUri;
+      imgCache[icon] = dataUri;
     }).catch(function (err) {
       console.log('Unable to load Pharah Icon', err);
     });
   }
 };
-ll();
+
+cacheMedia(url);
 
 var PharahApp = function (_Component) {
   inherits(PharahApp, _Component);
@@ -1218,35 +1223,34 @@ var PharahApp = function (_Component) {
 
 render(h(PharahApp, { githubUrl: githubUrl, url: url }), document.body);
 
-document.getElementById('pharah').addEventListener('click', function () {
+de('pharah').addEventListener('click', function () {
 
   function base64ToArrayBuffer(base64) {
     var binaryString = window.atob(base64);
     var len = binaryString.length;
     var bytes = new Uint8Array(len);
+    // bytes = bytes.map((b) => binaryString.charCodeAt(b))
+    // return bytes.buffer
     for (var i = 0; i < len; i++) {
       bytes[i] = binaryString.charCodeAt(i);
     }
     return bytes.buffer;
   }
 
-  var base64 = cache[url];
-  var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  var source = audioContext.createBufferSource();
-  audioContext.decodeAudioData(base64ToArrayBuffer(base64), function (buffer) {
-    source.buffer = buffer;
-    source.connect(audioContext.destination);
-    source.start(0);
-    source.onended = function () {
-      console.log('ended', source);
-      audioContext.close();
-    };
+  // var base64 = cache[url];
+  var url = mp3s[~~(Math.random() * mp3s.length)];
+  cacheAudio(url).then(function (base64) {
+    var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    var source = audioContext.createBufferSource();
+    audioContext.decodeAudioData(base64ToArrayBuffer(base64), function (buffer) {
+      source.buffer = buffer;
+      source.connect(audioContext.destination);
+      source.start(0);
+      source.onended = function () {
+        audioContext.close();
+      };
+    });
   });
-  audioContext.onended = function () {
-    console.log('ended', audioContext.state);
-    // audioContext.close()
-  };
-
 });
 
 }());
